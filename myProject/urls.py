e0b4ca42bc338bd5dbb63493273b0538c1d84625
yashsb-07 <yashsbansode07@gmail.com>
2026-04-24 15:00:17 +0530

    Learned How to add tailwind CSS

--- myProject/urls.py
+++ myProject/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('myApp/', include('myApp.urls') ),
+
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
