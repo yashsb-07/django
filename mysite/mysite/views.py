40b0f383c6e227124bebdec8c527b19d9d1984a3
yashsb-07 <yashsbansode07@gmail.com>
2026-06-12 09:54:47 +0530

    created and initialized templates

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse()
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("About Page")
