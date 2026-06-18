50a7bf87b17503467d1a81a78bfcbf4d44578bc8
yashsb-07 <yashsbansode07@gmail.com>
2026-06-18 10:57:17 +0530

    created database models for shop app

--- e_commerce/ecommerce/shop/views.py
+++ e_commerce/ecommerce/shop/views.py
@@ -4,3 +4,23 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render (request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse ('We are at about us page')
+
+def contact(request):
+    return HttpResponse ('We are at contact us page')
+ 
+def tracker(request):
+    return HttpResponse ('We are at tracker page')
+
+def search(request):
+    return HttpResponse ('We are at search page')
+    
+def productview(request):
+    return HttpResponse ('We are at product view page')
+    
+def checkout(request):
+    return HttpResponse ('We are at checkout page')
+
+    
