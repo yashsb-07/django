23144c5c525f80c39166ac83355b36af3244864b
yashsb-07 <yashsbansode07@gmail.com>
2026-06-19 09:45:10 +0530

    added template inheritance

--- e_commerce/ecommerce/shop/views.py
+++ e_commerce/ecommerce/shop/views.py
@@ -6,7 +6,7 @@ def index(request):
     return render (request, 'shop/index.html')
 
 def about(request):
-    return HttpResponse ('We are at about us page')
+    return render (request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse ('We are at contact us page')
