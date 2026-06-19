8533cb6a4972772d1d8e0cd9b619c80b20980d3f
yashsb-07 <yashsbansode07@gmail.com>
2026-06-19 10:53:01 +0530

    showing products on the home page

--- e_commerce/ecommerce/shop/views.py
+++ e_commerce/ecommerce/shop/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
+from django.http import HttpResponse
+
 def index(request):
-    return render (request, 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
-    return render (request, 'shop/about.html')
+    return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse ('We are at contact us page')
- 
+    return HttpResponse("We are at contact")
+
 def tracker(request):
-    return HttpResponse ('We are at tracker page')
+    return HttpResponse("We are at tracker")
 
 def search(request):
-    return HttpResponse ('We are at search page')
-    
-def productview(request):
-    return HttpResponse ('We are at product view page')
-    
-def checkout(request):
-    return HttpResponse ('We are at checkout page')
+    return HttpResponse("We are at search")
 
-    
+def productview(request):
+    return HttpResponse("We are at product view")
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
