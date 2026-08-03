ae4ccf3c27530694671e27866f252f49f56c1b52
yashsb-07 <yashsbansode07@gmail.com>
2026-08-03 08:01:38 +0530

    Update code

--- myApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.all_games, name='all_games'),
-    path('<int:game_id>/', views.game_details, name='game_details'),
-    path('game_store', views.game_store_view, name='game_store'),
-]
